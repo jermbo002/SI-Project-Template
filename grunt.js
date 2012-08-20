75e8465131a4964b63210a4d22160f7f106f490c
Jeremy Burton <jermbo002@gmail.com>
2012-08-20 11:49:49 -0500

    Add Compass node module and update normalize.css

--- grunt.js
+++ grunt.js
@@ -1,24 +1,43 @@
 ﻿/*global module:false*/
 module.exports = function( grunt ) {
+    'use strict';
 
-    // Project configuration.
+    // Grunt Configuration
+    // ------------------
     grunt.initConfig({
+
+        // Project configuration
+        // ---------------------
+
+        // package.json included in configuration
         pkg: {
             "name": "Project Name",
-            "app": "app dir",
+            "appDir": "app dir",
             "author": {
                 "name"  : "Jeremy Burton",
                 "email" : "jeremy@select-interactive.com",
                 "url"   : "www.select-interactive.com"
             }
         },
-        lint: {
-            files: ['grunt.js','js/<%= pkg.app %>/*.js']
+        
+        // compile .scss/.sass to .css using Compass
+        compass: {
+            dev: {
+                src: 'css/sass',
+                dest: 'css',
+                outputstyle: 'expanded',
+                linecomments: true
+            },
+            prod: {
+                src: 'css/sass',
+                dest: 'css',
+                outputstyle: 'compressed',
+                linecomments: false,
+                forcecompile: true
+            }
         },
-        watch: {
-            files: '<config:lint.files>',
-            tasks: 'lint'
-        },
+
+        // jhHint - set options
         jshint: {
             options: {
                 curly: true,
@@ -37,9 +56,33 @@ module.exports = function( grunt ) {
             globals: {
                 jQuery: true
             }
+        },
+
+        // js linting
+        lint: {
+            files: ['grunt.js','js/<%= pkg.appDir %>/*.js']
+        },
+
+        // watch task
+        watch: {
+            compass: {
+                files: [
+                    'css/sass/*.scss', 
+                    'css/sass/**/*.scss'
+                ],
+                tasks: 'compass:dev'
+            },
+
+            lint: {
+                files: ['grunt.js','js/<%= pkg.appDir %>/*.js'],
+                tasks: 'lint'
+            }
         }
     });
-
+    
     // Default task.
-    grunt.registerTask('default', 'lint');
+    grunt.registerTask( 'default', 'lint compass:prod' );
+
+    // Compass plugin task
+    grunt.loadNpmTasks( 'grunt-compass' );
 };
